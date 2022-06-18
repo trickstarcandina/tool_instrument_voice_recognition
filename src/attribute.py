e353b084430d578078d9a8144585b554c06f2830
trickstarcandina <rinnegan101220@gmail.com>
2022-06-18 23:18:10 +0700

    update detect

--- src/attribute.py
+++ src/attribute.py
@@ -2,8 +2,9 @@ from re import A
 
 
 class toolInstrumentVoice:
-    def __init__(self, a, b, c, d):
-        self.percentSilence = a
+    def __init__(self, a, b, c, d, e):
+        self.pitch = a
         self.RMSE = b
-        self.pitch = c
-        self.frequencyMagnitude = d
+        self.percentSilence = c
+        self.magnitude = d
+        self.frequency = e
--- src/detect.py
+++ src/detect.py
@@ -1,3 +1,9 @@
+from Pitch import funcPitch
+from aubio import pitch
+from RMSE import funcRMSE
+from PercentSilence import funcPercentSilence
+from FrequencyMagnitude import funcFrequencyMagnitude
+
 ### % phần trăm khoảng lặng 0,1
 ### độ lệch năng lượng 0,3
 ### pitch 0,3
@@ -7,7 +13,7 @@ from attribute import toolInstrumentVoice
 configPercentSilence = 0.1
 configRMSE = 0.3
 configPitch = 0.3
-configPercentSilence = 0.3
+configFrequencyMagnitude = 0.3
 
 def compareFile(att1, att2): 
     maxRes = 0
@@ -17,31 +23,52 @@ def compareFile(att1, att2):
             same = float(abs(att1[i].percentSilence - att2[j].percentSilence) / max(att1[i].percentSilence, att2[j].percentSilence) * configPercentSilence)
             same = same + float(abs(att1[i].RMSE - att2[j].RMSE) / max(att1[i].RMSE, att2[j].RMSE) * configRMSE)
             same = same + float(abs(att1[i].pitch - att2[j].pitch) / max(att1[i].pitch, att2[j].pitch) * configPitch)
-            same = same + float(abs(att1[i].frequencyMagnitude - att2[j].frequencyMagnitude) / max(att1[i].frequencyMagnitude, att2[j].frequencyMagnitude) * configPercentSilence)
-            if(maxRes < same):
+            same = same + float(abs(att1[i].magnitude - att2[j].magnitude) / max(att1[i].magnitude, att2[j].magnitude) * configFrequencyMagnitude / 2)
+            same = same + float(abs(att1[i].frequency - att2[j].frequency) / max(att1[i].frequency, att2[j].frequency) * configFrequencyMagnitude / 2)
+            if(maxRes > same):
                 maxRes = same
     
     return maxRes
     
 # input
+path = "E:/Learn/tool_instrument_voice_recognition/src/File âm thanh/1 máy sấy tóc/may_say_toc_1.wav"
 att1 = [] 
-# for i in range(7):
-att1.append(toolInstrumentVoice(54.6511, 138, 32, 520))
-att1.append(toolInstrumentVoice(61.7545, 116.8, 33, 815))
-att1.append(toolInstrumentVoice(58.1165, 72, 33.5, 1500))
-att1.append(toolInstrumentVoice(49.8548, 107.7, 34, 2255))
-att1.append(toolInstrumentVoice(53.3301, 120, 35, 2556))
-att1.append(toolInstrumentVoice(57.3430, 95, 35.6, 2867))
-att1.append(toolInstrumentVoice(62.4325, 91, 37, 520))
 
+pitchAtt = funcPitch(path, pitch)
+RMSEAtt = funcRMSE(path)
+percentSilenceAtt = funcPercentSilence(path)
+frequencyMagnitudeAtt = funcFrequencyMagnitude(path)
+
+magnitudeAtt = []
+frequencyAtt = []
+
+for a, b in frequencyMagnitudeAtt:
+    magnitudeAtt.append(a)
+    frequencyAtt.append(b)
+
+for i in range(7):
+    att1.append(
+        toolInstrumentVoice(
+            pitchAtt[i],
+            RMSEAtt[i],
+            percentSilenceAtt[i],
+            magnitudeAtt[i],
+            frequencyAtt[i]
+        )
+    )
+    print (pitchAtt[i], RMSEAtt[i], percentSilenceAtt[i], magnitudeAtt[i], frequencyAtt[i])
 # có sẵn
+# "[, , , , , , ]",
+# "[, , , , , , ]",
+# "[, , , , , , ]",
+# "[(), (), (), (), (), (), ()]"
 att2 = []
-att2.append(toolInstrumentVoice(40, 144, 2, 550))
-att2.append(toolInstrumentVoice(41.7545, 123, 3, 315))
-att2.append(toolInstrumentVoice(42.1165, 98, 5, 150))
-att2.append(toolInstrumentVoice(43.8548, 84, 4, 225))
-att2.append(toolInstrumentVoice(44.3301, 115, 10, 280))
-att2.append(toolInstrumentVoice(45.3430, 93, 15, 267))
-att2.append(toolInstrumentVoice(46.4325, 97, 17, 820))
+att2.append(toolInstrumentVoice(41.0105, 52.480352808569755, 18.498866213151928, 6044, 752.2972848700547))
+att2.append(toolInstrumentVoice(80.53759, 6.295327728661332, 48.49433106575963, 25529, 144.3187829958743))
+att2.append(toolInstrumentVoice(78.17408, 3.6278046603824587, 47.73242630385488, 45436, 66.06745432748225))
+att2.append(toolInstrumentVoice(79.63045, 3.843097218001882, 48.29478458049886, 70831, 37.561908261746865))
+att2.append(toolInstrumentVoice(78.884094, 7.780515449953133, 49.88208616780045, 108914, 66.06745432748225))
+att2.append(toolInstrumentVoice(80.22667, 5.244177334922521, 49.55555555555555, 128821, 144.3187829958743))
+att2.append(toolInstrumentVoice(80.14571, 7.7701177481295804, 45.35514878292073, 148306, 752.2972848700547))
 
 print(compareFile(att1, att2))
